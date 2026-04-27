1e63e46a91d3de84334e891ddb80e544f3e84e23
hegert <htaresalu@proton.me>
2026-04-27 21:43:55 +0300

    add typescript lsp

--- init.lua
+++ init.lua
@@ -697,6 +697,16 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+
+        json = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        markdown = { 'prettier' },
+
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
